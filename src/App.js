65765e9aa853325a136c53764f50f49ea0681080
Ryan Morash <ryan@morash.me>
2021-03-28 20:57:39 -0400

    Add pagination
    Added useTokenPagination - not completely sure if it actually works

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import './App.scss';
 import logo from './logo.svg';
 
-import { useTable } from 'react-table';
+import { useTokenPagination, useTable } from 'react-table';
 import ReactMarkdown from 'react-markdown';
 
 import Container from 'react-bootstrap/Container';
@@ -24,41 +24,52 @@ Amplify.configure(awsExports);
 
 function Table({ columns, data }) {
   // Use the state and functions returned from useTable to build your UI
-  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow } = useTable({
+  const { getTableProps, getTableBodyProps, headerGroups, rows, prepareRow, previousPage, nextPage, canNextPage, canPreviousPage } = useTable({
     columns,
     data,
-  })
+  },
+  useTokenPagination)
 
   return (
-    <BTable striped bordered hover {...getTableProps()}>
-      <thead>
-        {headerGroups.map(headerGroup => (
-          <tr {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map(column => (
-              <th {...column.getHeaderProps()}>
-                {column.render('Header')}
-              </th>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map(row => {
-            prepareRow(row)
-            return (
-            <tr {...row.getRowProps()}>
-              {row.cells.map(cell => {
-                return (
-                  <td {...cell.getCellProps()}>
-                  {cell.render('Cell')}
-                  </td>
-                )
-              })}
+    <>
+      <BTable striped bordered hover {...getTableProps()}>
+        <thead>
+          {headerGroups.map(headerGroup => (
+            <tr {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map(column => (
+                <th {...column.getHeaderProps()}>
+                  {column.render('Header')}
+                </th>
+              ))}
             </tr>
-          )
-        })}
-      </tbody>
-    </BTable>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map(row => {
+              prepareRow(row)
+              return (
+              <tr {...row.getRowProps()}>
+                {row.cells.map(cell => {
+                  return (
+                    <td {...cell.getCellProps()}>
+                    {cell.render('Cell')}
+                    </td>
+                  )
+                })}
+              </tr>
+            )
+          })}
+        </tbody>
+      </BTable>
+      <div className="pagination d-flex justify-content-between">
+        <Button onClick={() => previousPage()} disabled={!canPreviousPage}>
+          Previous Page
+        </Button>
+        <Button onClick={() => nextPage()} disabled={!canNextPage}>
+          Next Page
+        </Button>
+      </div>
+    </>
   )
 
 }
